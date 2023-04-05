22a0336d8c7e4db27801d1bfefa0d0ca10f06fc8
daonguyendev <daonguyen.dev@gmail.com>
2023-04-05 16:30:28 +0700

    Init commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import AddComponent from './components/AddComponents';
+
+function App() {
+  return (
+    <AddComponent firstNumber={1} secondNumber={2}/>
+  );
+}
+
+export default App;
